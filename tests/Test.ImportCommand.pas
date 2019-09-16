493ea9d7be1c65a86c0cb660c626be378f566483
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-09-17 00:13:19 +0100

    Created empty test project

---
+++ tests/Test.ImportCommand.pas
@@ -0,0 +1,46 @@
+﻿unit Test.ImportCommand;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Classes, System.SysUtils;
+
+{$M+}
+
+type
+  [TestFixture]
+  TImportCommandFixture = class(TObject)
+  private
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+  published
+    procedure Test1;
+  end;
+
+implementation
+
+uses
+  System.Variants;
+
+
+procedure TImportCommandFixture.Setup;
+begin
+
+end;
+
+procedure TImportCommandFixture.TearDown;
+begin
+
+end;
+
+
+procedure TImportCommandFixture.Test1;
+begin
+
+end;
+
+end.
